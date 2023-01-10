4736bfa02e1006ae00c752daea70f22edd896cd1
Amir Gambo Ibrahim <gambo.amir@yahoo.com>
2023-01-10 12:49:56 -0800

    Python - Input/Output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,4 +1,4 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 """Reads from standard input and computes metrics.
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
