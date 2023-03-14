b2a6b03fbfc678bd34dbd4de8ecef4f494a65f9f
Amir Gambo Ibrahim <gambo.amir@yahoo.com>
2023-03-14 16:20:56 -0700

    JavaScript - Objects, Scopes and Closures

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Class Rectangle that defines rectangle
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+};
